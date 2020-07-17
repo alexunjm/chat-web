1fb3f7270401b7a138f04b431cc5d0724092ceba
alexanderjaramillo <alexunjm@gmail.com>
2020-07-17 14:47:41 -0500

    side bar

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
     <app-top-bar class="h-8 w-full">
     </app-top-bar> -->
     <!-- main -->
-    <div class="relative h-full sm:block">
-      <app-side-bar class="w-48 h-full absolute t-0 transform -translate-x-full sm:translate-x-0">
+    <div class="relative min-h-screen sm:block">
+      <app-side-bar class="w-48 h-full bg-gray-200 text-blue-600 absolute t-0 transform -translate-x-full sm:translate-x-0">
       </app-side-bar>
       <div class="chat-main h-full flex flex-col sm:flex-grow sm:pl-48 sm:border-box">
         <router-outlet></router-outlet>
